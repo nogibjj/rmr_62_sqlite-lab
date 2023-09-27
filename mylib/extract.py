e53aad730ea4350200570151d9e4f0fd76c8dea5
rmr327 <36940292+rmr327@users.noreply.github.com>
2023-09-27 18:39:47 +0000

    test

--- mylib/extract.py
+++ mylib/extract.py
@@ -1,11 +1,13 @@
 """
-Extract a dataset from a URL like Kaggle or data.gov. JSON or CSV formats tend to work well
+Extract a dataset from a URL like Kaggle or data.gov. JSON or CSV formats 
+tend to work well
 
 food dataset
 """
 import requests
 
-def extract(url="https://raw.githubusercontent.com/Barabasi-Lab/GroceryDB/main/data/GroceryDB_IgFPro.csv", 
+def extract(url="https://raw.githubusercontent.com/Barabasi-Lab/"
+            "GroceryDB/main/data/GroceryDB_IgFPro.csv", 
             file_path="data/GroceryDB_IgFPro.csv"):
     """"Extract a url to a file path"""
     with requests.get(url) as r:
--- mylib/transform_load.py
+++ mylib/transform_load.py
@@ -1,7 +1,9 @@
 """
 Transforms and Loads data into the local SQLite3 database
 Example:
-,general name,count_products,ingred_FPro,avg_FPro_products,avg_distance_root,ingred_normalization_term,semantic_tree_name,semantic_tree_node
+,general name,count_products,ingred_FPro,avg_FPro_products,
+avg_distance_root,ingred_normalization_term,semantic_tree_name,
+semantic_tree_node
 """
 import sqlite3
 import csv
@@ -17,10 +19,11 @@ def load(dataset="data/GroceryDB_IgFPro.csv"):
     conn = sqlite3.connect('GroceryDB.db')
     c = conn.cursor()
     c.execute("DROP TABLE IF EXISTS GroceryDB")
-    c.execute("CREATE TABLE GroceryDB (id,general_name, count_products, ingred_FPro, avg_FPro_products, avg_distance_root, ingred_normalization_term, semantic_tree_name, semantic_tree_node)")
+    c.execute("CREATE TABLE GroceryDB (id,general_name, count_products," 
+              "ingred_FPro, avg_FPro_products, avg_distance_root," 
+              "ingred_normalization_term, semantic_tree_name, semantic_tree_node)")
     #insert
     c.executemany("INSERT INTO GroceryDB VALUES (?,?, ?, ?, ?, ?, ?, ?, ?)", payload)
     conn.commit()
     conn.close()
     return "GroceryDB.db"
-
