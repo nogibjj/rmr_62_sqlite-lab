c27be90e55d0a873bcdd039e1acd161c53dc5ef0
rmr327 <36940292+rmr327@users.noreply.github.com>
2023-09-27 18:28:29 +0000

    init

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
-"""
-ETL-Query script
-"""
-
+import argparse
 from mylib.extract import extract
 from mylib.transform_load import load
 from mylib.query import query
 
-# Extract
-print("Extracting data...")
-extract()
+parser = argparse.ArgumentParser(description='Process some data.')
+parser.add_argument('-e', '--extract', action='store_true', help='Extract data')
+parser.add_argument('-l', '--load', action='store_true', help='Load data')
+parser.add_argument('-q', '--query', action='store_true', help='Query data')
 
-# Transform and load
-print("Transforming data...")
-load()
+args = parser.parse_args()
 
-# Query
-print("Querying data...")
-query()
+if args.extract:
+    print("Extracting data...")
+    extract()
+
+if args.load:
+    print("Loading data...")
+    load()
+
+if args.query:
+    print("Querying data...")
+    query()
--- mylib/query.py
+++ mylib/query.py
@@ -1,15 +1,21 @@
 """Query the database"""
 
 import sqlite3
+from prettytable import PrettyTable
 
 
 def query():
     """Query the database for the top 5 rows of the GroceryDB table"""
     conn = sqlite3.connect("GroceryDB.db")
     cursor = conn.cursor()
-    cursor.execute("SELECT * FROM GroceryDB")
+    cursor.execute("SELECT * FROM GroceryDB LIMIT 5")
     print("Top 5 rows of the GroceryDB table:")
-    print(cursor.fetchall())
+    rows = cursor.fetchall()
+    x = PrettyTable()
+    x.field_names = [i[0] for i in cursor.description]
+    for row in rows:
+        x.add_row(row)
+    print(x)
     conn.close()
     return "Success"
 
